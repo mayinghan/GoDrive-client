ac0bcc699bfb23ce2ce65b1a8165862aaf10a8dd
Xinxin0407 <wuxinxin0407@gmail.com>
2020-02-11 19:46:47 -0500

    'add_images_in_homepage'

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import thunk from 'redux-thunk'; // for async dispatcher
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import AuthRoute from './Router/AuthRoute.react';
-import './App.css';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reducers from './reducers';
+import React from "react";
+import thunk from "redux-thunk"; // for async dispatcher
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import AuthRoute from "./Router/AuthRoute.react";
+import "./App.css";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducers from "./reducers";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const store = createStore(
-	reducers,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__
-			? window.__REDUX_DEVTOOLS_EXTENSION__()
-			: f => f
-	)
+  reducers,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  )
 );
 
 function App() {
-	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				<div>
-					<AuthRoute></AuthRoute>
-				</div>
-			</BrowserRouter>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <div>
+          <AuthRoute></AuthRoute>
+        </div>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
