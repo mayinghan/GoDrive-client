8f1d8c80059b7ebc0c280af6964c9d72132e19bc
“mayinghan” <ma.yingha@ufl.edu>
2020-03-12 22:54:31 -0400

    resolve file conflicts

--- src/container/FileContainer/FileUpload.react.js
+++ src/container/FileContainer/FileUpload.react.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Upload, Progress, message } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
